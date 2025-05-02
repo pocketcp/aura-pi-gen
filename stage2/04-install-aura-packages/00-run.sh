114cdb3e6a7628f49f6389d4cd511c7fe9a8e8b7
Chaithanya Padi <chaithanya@amahahealth.com>
2025-05-02 23:42:23 +0530

    custom bootscreen

--- stage2/04-install-aura-packages/00-run.sh
+++ stage2/04-install-aura-packages/00-run.sh
@@ -11,3 +11,22 @@ echo "Welcome to Aura OS" > /etc/motd
 
 # Example: Enable a systemd service if you ship one
 # systemctl enable aura.service
+
+# Define theme directory and files
+THEME_NAME="project-oriel"
+THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
+PROJECT_DIR="$(pwd)/stage2/04-install-aura-packages/$THEME_NAME-theme"
+
+# Create theme directory
+sudo mkdir -p "$THEME_DIR"
+
+# Copy theme files
+sudo cp "$PROJECT_DIR"/* "$THEME_DIR"
+
+# Set the theme as default
+sudo plymouth-set-default-theme -R "$THEME_NAME"
+
+# Update initramfs
+sudo update-initramfs -u
+
+echo "Plymouth theme '$THEME_NAME' has been set up successfully."
