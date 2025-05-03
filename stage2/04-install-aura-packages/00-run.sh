6990697e23aca782726d40929e99e8a4f61243b0
Chaithanya Padi <chaithanya@amahahealth.com>
2025-05-03 09:13:54 +0530

    no theme for now

--- stage2/04-install-aura-packages/00-run.sh
+++ stage2/04-install-aura-packages/00-run.sh
@@ -12,21 +12,21 @@ echo "Welcome to Aura OS" > /etc/motd
 # Example: Enable a systemd service if you ship one
 # systemctl enable aura.service
 
-# Define theme directory and files
-THEME_NAME="project-oriel-theme"
-THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
-PROJECT_DIR="$(pwd)/stage2/04-install-aura-packages/$THEME_NAME"
+# # Define theme directory and files
+# THEME_NAME="project-oriel-theme"
+# THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
+# PROJECT_DIR="$(pwd)/stage2/04-install-aura-packages/$THEME_NAME"
 
-# Create theme directory
-mkdir -p "$THEME_DIR"
+# # Create theme directory
+# mkdir -p "$THEME_DIR"
 
-# Copy theme files
-cp "$PROJECT_DIR"/* "$THEME_DIR"
+# # Copy theme files
+# cp "$PROJECT_DIR"/* "$THEME_DIR"
 
-# Set the theme as default
-plymouth-set-default-theme -R "$THEME_NAME"
+# # Set the theme as default
+# plymouth-set-default-theme -R "$THEME_NAME"
 
-# Update initramfs
-update-initramfs -u
+# # Update initramfs
+# update-initramfs -u
 
-echo "Plymouth theme '$THEME_NAME' has been set up successfully."
+# echo "Plymouth theme '$THEME_NAME' has been set up successfully."
