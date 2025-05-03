49433a40f54ed689cf112bb62dd0ac72837f00d1
Chaithanya Padi <chaithanya@amahahealth.com>
2025-05-03 06:21:44 +0530

    checking the run

--- stage2/04-install-aura-packages/00-run.sh
+++ stage2/04-install-aura-packages/00-run.sh
@@ -18,15 +18,15 @@ THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
 PROJECT_DIR="$(pwd)/stage2/04-install-aura-packages/$THEME_NAME-theme"
 
 # Create theme directory
-sudo mkdir -p "$THEME_DIR"
+mkdir -p "$THEME_DIR"
 
 # Copy theme files
-sudo cp "$PROJECT_DIR"/* "$THEME_DIR"
+cp "$PROJECT_DIR"/* "$THEME_DIR"
 
 # Set the theme as default
-sudo plymouth-set-default-theme -R "$THEME_NAME"
+plymouth-set-default-theme -R "$THEME_NAME"
 
 # Update initramfs
-sudo update-initramfs -u
+update-initramfs -u
 
 echo "Plymouth theme '$THEME_NAME' has been set up successfully."
